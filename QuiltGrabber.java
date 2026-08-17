04a13d21888df35f62e1fc98ddf2b64003b2a45e
Harley Hannahs <hanna537@morris.umn.edu>
2026-08-17 16:05:37 -0500

    Add quiltSize variable for DQImagePanelAbstract construction
    quiltSize variable can now be changed to draw any size of grid on the image input into QuiltGrabber

--- QuiltGrabber.java
+++ QuiltGrabber.java
@@ -7,15 +7,16 @@ public class QuiltGrabber {
 
  public static void main( String args[]) {
   BufferedImage bimg = null;
+  int quiltSize = 2;
   try {
-    bimg = ImageIO.read( new File("test.jpg"));
+    bimg = ImageIO.read( new File("IMG_1932.JPG"));
       } catch( Exception e) {
 	e.printStackTrace();
  	}
    System.out.println("Click the four corners of the DigiQuilt.");
    System.out.println("Start at the top left, then work your way clockwise!");
 	
-   DQImagePanel dqip = new DQImagePanel(bimg);
+   DQImagePanelAbstract dqip = new DQImagePanelAbstract(bimg, quiltSize);
    JFrame myFrame = new JFrame("QuiltGrabber");
    myFrame.setBounds(0,0,bimg.getWidth(), bimg.getHeight());
    myFrame.add(dqip);
