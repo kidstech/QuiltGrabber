13585f10f592d949a9b59748fad5806e4ef9549a
imbroglio-0 <hanna537@morris.umn.edu>
2026-09-09 01:41:05 -0500

    Add viewer files for DigiQuilt PNG generation
    While the user can't see these displays, they still need to exist in order to correctly paint the quilt onto a file. These operations simply happen in memory rather than in front of the user.

---
+++ src/main/java/kidstech/pngGeneration/GridDisplayer.java
@@ -0,0 +1,13 @@
+package kidstech.pngGeneration;
+
+import kidstech.quiltObjects.Grid;
+
+// referenced from DigiQuilt
+public interface GridDisplayer {
+    /**
+     * Sets the current Grid to the given set.
+     * 
+     * @param newLines
+     */
+    void setGrid(Grid newLines);
+}
