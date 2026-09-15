e548f2883a79d9d74e47caac83579d12efe79e80
Harley Hannahs <hanna537@morris.umn.edu>
2026-09-15 13:34:44 -0500

    Add more quilt images
    More quilt images are added to the test resources folder, as well as updating the QuiltGrabber.java main file

--- src/main/java/kidstech/QuiltGrabber.java
+++ src/main/java/kidstech/QuiltGrabber.java
@@ -8,10 +8,10 @@ public class QuiltGrabber {
 
  public static void main( String args[]) {
   BufferedImage bimg = null;
-  // quiltSize is number of blocks per one side of the quilt (NOT PATCHES)
-  int quiltSize = 4;
+  // quiltSize is number of patches per one side of the quilt (standard DigiQuilt sizes are 2x2, 3x3, 4x4)
+  int quiltSize = 3;
   try {
-    bimg = ImageIO.read( new File("src/test/resources/QuiltPhotos/IMG_1932.JPG"));
+    bimg = ImageIO.read( new File("src/test/resources/QuiltPhotos/IMG_1928.JPG"));
   } catch( Exception e) {
     e.printStackTrace();
  	}
